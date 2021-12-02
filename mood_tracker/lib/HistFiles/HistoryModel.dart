0d326bd64786eb316da487c89e1d96d2129e7767
Zarxong <che.baptiste37@gmail.com>
2021-12-01 22:02:34 -0500

    d

--- mood_tracker/lib/HistFiles/HistoryModel.dart
+++ mood_tracker/lib/HistFiles/HistoryModel.dart
@@ -1,7 +1,9 @@
 class HistoryModel {
-  final String title, descrip;
+  final String title; 
+  final String descrip;
   final double Q1, Q2, Q3, Q4, Q5;
 
   HistoryModel(
-      this.title, this.descrip, this.Q1, this.Q2, this.Q3, this.Q4, this.Q5);
+      this.title, 
+     this.descrip, this.Q1, this.Q2, this.Q3, this.Q4, this.Q5,);
 }
