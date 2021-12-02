9160b1cc445a959e79329ba0283480f52330d7a4
Zarxong <che.baptiste37@gmail.com>
2021-12-01 23:34:56 -0500

    HistoryUpd

--- mood_tracker/lib/HistFiles/HistDetails.dart
+++ mood_tracker/lib/HistFiles/HistDetails.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mood_tracker/BarChart/mood_chart.dart';
 import 'HistoryModel.dart';
 import 'package:mood_tracker/BarChart/bar_chart_data.dart';
 
@@ -22,12 +23,12 @@ class HistDetails extends StatelessWidget {
       ),
       child: Scaffold(
         appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: Text(historyModel.title),
-        elevation: 0,
+          backgroundColor: Colors.transparent,
+          title: Text(historyModel.title),
+          elevation: 0,
         ),
         backgroundColor: Colors.transparent,
-          //title: Text(historyModel.title),
+        //title: Text(historyModel.title),
 
         body: ListView(
           padding: const EdgeInsets.all(32),
@@ -36,41 +37,60 @@ class HistDetails extends StatelessWidget {
             //const SizedBox(height: 40),
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [ 
-                const SizedBox(height: 20),
-                     Text(historyModel.descrip,
-                    style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)
-                    ),
-                    const SizedBox(height: 20),
-                     Text(historyModel.Q1.toString(),
-                    style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)
-                    ),
-                    const SizedBox(height: 20),
-                     Text(historyModel.Q2.toString(),
-                    style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)
-                    ),
-                    const SizedBox(height: 20),
-                     Text(historyModel.Q3.toString(),
-                    style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)
-                    ),
-                    const SizedBox(height: 20),
-                     Text(historyModel.Q4.toString(),
-                    style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)
-                    ),
-                    const SizedBox(height: 20),
-                     Text(historyModel.Q5.toString(),
-                    style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)
-                    ),
-            
-            
-            // Text('testing helloooo'),
-            // Text(historyModel.Q1.toString()),
-            // Text(historyModel.Q2.toString()),
-            // Text(historyModel.Q3.toString()),
-            // Text(historyModel.Q4.toString()),
-            // Text(historyModel.Q5.toString()),
-            ],),
-                    
+              children: [
+                const SizedBox(height: 10),
+
+                SizedBox(
+                  width: 356,
+                  height: 250,
+                  child: MoodChart(data: [
+                    historyModel.Q1,
+                    historyModel.Q2,
+                    historyModel.Q3,
+                    historyModel.Q4
+                  ]),
+                ),
+                const SizedBox(height: 30),
+                const Text('Journal Entry:',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)),
+const SizedBox(height: 30),
+                        Text(historyModel.descrip,
+                    style: const TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)),
+                
+                //  Text(historyModel.Q1.toString(),
+                // style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)
+                // ),
+                // const SizedBox(height: 20),
+                //  Text(historyModel.Q2.toString(),
+                // style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)
+                // ),
+                // const SizedBox(height: 20),
+                //  Text(historyModel.Q3.toString(),
+                // style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)
+                // ),
+                // const SizedBox(height: 20),
+                //  Text(historyModel.Q4.toString(),
+                // style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)
+                // ),
+                // const SizedBox(height: 20),
+                //  Text(historyModel.Q5.toString(),
+                // style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)
+                // ),
+
+                // Text('testing helloooo'),
+                // Text(historyModel.Q1.toString()),
+                // Text(historyModel.Q2.toString()),
+                // Text(historyModel.Q3.toString()),
+                // Text(historyModel.Q4.toString()),
+                // Text(historyModel.Q5.toString()),
+              ],
+            ),
           ],
         ),
       ),
