d913ec1c697b23766f8a8159fd5893151ecc219a
rbugge <ryan.bugge@tuta.io>
2021-12-08 13:21:34 -0500

    testing

--- mood_tracker/lib/api/testing.dart
+++ mood_tracker/lib/api/testing.dart
@@ -1,43 +1,22 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
+// Import the test package and Counter class
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mood_tracker/login.dart';
+import 'package:mood_tracker/home.dart';
 
+void main() {
+  test('login should return', () {
+    final counter = loadData();
 
-class test extends StatelessWidget{
+    counter.increment();
 
-  @override
-  Widget build (BuildContext context){
-    return MaterialApp(
-      title: 'testing',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('testing'),
-        ),
+    expect(counter.value, 1);
+  });
 
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => FirebaseFirestore.instance.collection('testing').add({'timestamp': Timestamp.fromDate(DateTime.now())}),
-          child: Icon(Icons.add),
-        ),
-        body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('testing').snapshots(),
-          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot,){
-            if (!snapshot.hasData) return const SizedBox.shrink();
-            return ListView.builder(
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (BuildContext context, int index){
-                final docData = snapshot.data!.docs[index].data();
-                //final dateTime = (docData['timestamp'] as Timestamp).toDate();
-                return ListTile(
-                  title: Text(docData.toString()),
-                );
-              }
-            );
-          },
-        ),
-      ),
-    );
-  }
+  // test('Entries should be found', () {
+  //   final counter = Counter();
+
+  //   counter.increment();
+
+  //   expect(counter.value, 1);
+  // });
 }
